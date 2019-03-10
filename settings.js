32bf577dc187a48a9f7c7a7db7c0048be2c6c2cb
Jonas Weinz <s6jowein@uni-bonn.de>
2019-03-10 16:33:55 +0100

    little fixes

--- settings.js
+++ settings.js
@@ -1,2 +1,4 @@
+var home = "https://the-cake-is-a-lie.net/website/ultimate_tictactoe/";
+var rel_home = "/website/ultimate_tictactoe";
 var server_url = "the-cake-is-a-lie.net";
 var server_port = "5555";
